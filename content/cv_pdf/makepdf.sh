b37d21a2f7a14d14b776e4fd82bfe32e7b746816
Florian Wagner <mail@fwagner.info>
2016-04-13 22:55:17 +0200

    pandoc smart option

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-pandoc ../pages/publications.rst -o publications.tex
+pandoc -S ../pages/publications.rst -o publications.tex
 sed -i '1,2d' publications.tex
 sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
-latexmk -pdf cv.tex
 latexmk -pdf cv.tex && mv -f cv.pdf ../static/cv_fwagner.pdf
 
