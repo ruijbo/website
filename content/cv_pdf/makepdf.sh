1693c6e0f79ff585e74ad9dd8165d4515d59c777
Florian Wagner <mail@fwagner.info>
2016-04-13 18:34:36 +0200

    remove texfot

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -6,5 +6,5 @@ sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
 sed -i 's/^$/\\vspace{0.25cm}\n/g' publications.tex
-texfot pdflatex cv.tex && mv -f cv.pdf ../static/cv_fwagner.pdf
+pdflatex cv.tex && mv -f cv.pdf ../static/cv_fwagner.pdf
 
