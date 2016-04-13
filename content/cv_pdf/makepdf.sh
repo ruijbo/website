74b9464629ff4c4ef0cb5f2f097854fe21a58034
Florian Wagner <mail@fwagner.info>
2016-04-13 22:25:32 +0200

    noninteractive mode latex

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -5,6 +5,7 @@ sed -i '1,2d' publications.tex
 sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
-sed -i 's/^$/\\vspace{0.25cm}\n/g' publications.tex
-pdflatex cv.tex && mv cv.pdf ../static/cv_fwagner.pdf
+sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
+pdflatex -interaction=nonstopmode cv.tex
+pdflatex -interaction=nonstopmode cv.tex && mv -f cv.pdf ../static/cv_fwagner.pdf
 
