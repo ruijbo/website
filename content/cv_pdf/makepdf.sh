6973c5157730e5fee09f0e0782df73d7a9071ffe
Florian Wagner <mail@fwagner.info>
2016-04-14 09:49:47 +0200

    move pdf to download folder

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+tex="pdflatex -halt-on-error -interaction=nonstopemode cv.tex"
+
 pandoc -S ../pages/publications.rst -o publications.tex
 sed -i '1,2d' publications.tex
 sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
-(pdflatex -interaction=nonstopmode cv.tex && pdflatex -interaction=nonstopmode cv.tex && pdflatex -interaction=nonstopmode cv.tex) || echo Compilation failed
+($tex && $tex && $tex) || echo "PDF compilation failed."
+if [[ -f cv.pdf ]]; then
+    echo "Moving new PDF to download folder."
+    mv -f cv.pdf ../static/cv_fwagner.pdf
+fi
