1b03420f79e102a0b2b152eaaf7b229df412c55b
Florian Wagner <mail@fwagner.info>
2016-04-14 11:17:31 +0200

    raw html fix

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-tex="pdflatex -interaction=nonstopemode cv.tex"
+tex="pdflatex -interaction=nonstopmode cv.tex"
 
 pandoc ../pages/publications.rst -o publications.tex
+
 sed -i '1,2d' publications.tex
 sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
-sed -i 's/:raw-html:.*\n/\n/g' publications.tex
+
+# Delete raw html (newer pandoc versions do it automatically)
+sed -i 's/:raw-html:/\n:raw-html:/g' publications.tex
+sed -i '/:raw-html:/,/<\/a>/d' publications.tex
+
 ($tex && $tex && $tex) || echo "PDF compilation failed."
 if [[ -f cv.pdf ]]; then
     echo "Moving new PDF to download folder."
