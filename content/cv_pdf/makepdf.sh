e5e3d52235fe2c8effc79d8f3abae4908d476ad4
Florian Wagner <mail@fwagner.info>
2016-04-14 10:13:11 +0200

    remove raw html

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -2,12 +2,13 @@
 
 tex="pdflatex -interaction=nonstopemode cv.tex"
 
-pandoc -S ../pages/publications.rst -o publications.tex
+#pandoc ../pages/publications.rst -o publications.tex
 sed -i '1,2d' publications.tex
 sed -i '/quote/d' publications.tex
 sed -i 's/\\sub/\\/g' publications.tex
 sed -i 's/\\subsection/\\textbf/g' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
+sed -i 's/:raw-html:.*$//g' publications.tex
 ($tex && $tex && $tex) || echo "PDF compilation failed."
 if [[ -f cv.pdf ]]; then
     echo "Moving new PDF to download folder."
