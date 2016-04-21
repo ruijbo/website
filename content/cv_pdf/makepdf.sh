fc4fb33a93736225293aa130ea3ed26062c5f039
Florian Wagner <mail@fwagner.info>
2016-04-21 20:34:31 +0200

    minor change in pdf

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -14,6 +14,12 @@ sed -i 's/:raw-html:/\n\n:raw-html:/g' publications.tex
 sed -i '/:raw-html:/,/^$/d' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
 
+# Make CO2 subscript
+sed -i 's/CO2/CO\\textsubscript{2}/g' publications.tex
+sed -i 's/1st/1\\textsuperscript{st}/g' publications.tex
+sed -i 's/2nd/2\\textsuperscript{nd}/g' publications.tex
+sed -i 's/3rd/3\\textsuperscript{rd}/g' publications.tex
+
 ($tex && $tex && $tex) || echo "PDF compilation failed."
 if [[ -f cv.pdf ]]; then
     echo "Moving new PDF to download folder."
