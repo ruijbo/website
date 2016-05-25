85fdd478e707f4cb58efb42766271860fcb0a1f4
Florian Wagner <mail@fwagner.info>
2016-05-25 09:19:26 +0200

    fix subscript

--- content/cv_pdf/makepdf.sh
+++ content/cv_pdf/makepdf.sh
@@ -15,7 +15,7 @@ sed -i '/:raw-html:/,/^$/d' publications.tex
 sed -i 's/^$/\\vspace{0.22cm}\n/g' publications.tex
 
 # Make CO2 subscript
-#sed -i 's/CO2/CO\\textsubscript{2}/g' publications.tex
+sed -i 's/CO2/CO\\textsubscript{2}/g' publications.tex
 sed -i 's/1st/1\\textsuperscript{st}/g' publications.tex
 sed -i 's/2nd/2\\textsuperscript{nd}/g' publications.tex
 sed -i 's/3rd/3\\textsuperscript{rd}/g' publications.tex
